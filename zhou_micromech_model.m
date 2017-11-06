1395db76c30840b27e455767b07ab246d17f1c35
Vanessa Meschke <vmeschke@wisc.edu>
2017-11-06 10:58:50 -0600

    Micromechanical Model Comparison
    Running the WISDOT report data through Zhou's Micromechanical model
    proposed in this paper:
    http://ascelibrary.org/doi/10.1061/%28ASCE%29MT.1943-5533.0000663

---
+++ zhou_micromech_model.m
@@ -0,0 +1,12 @@
+check = 0;
+% Coarse Aggregate type: 0 - GG, 1 - CS
+aggregate_type = [0,1];
+% Sieve Size (mm)
+sieve_size = [37.5, 25, 19, 12,5, 9.5, 4.75, 2.36];
+
+while check == 0
+   % SUMMATION OVER COARSE AGGREGATE SIZE
+   for sieve_diam = sieve_size
+       
+   end
+end
